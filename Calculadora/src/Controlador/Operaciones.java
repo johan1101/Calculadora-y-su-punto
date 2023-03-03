c1a93c6f80fa2515ec47dc74a9f7950637b10a6c
johan1101 <118584996+johan1101@users.noreply.github.com>
2023-03-02 21:20:25 -0500

    Inicio
    Odio el punto

---
+++ Calculadora/src/Controlador/Operaciones.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controlador;
+
+/**
+ *
+ * @author sistemas
+ */
+public class Operaciones {
+    public String Sumar(float numero1, float numero2){
+        float resultado = numero1+numero2;
+        return String.valueOf(resultado);
+    }
+    
+    public String Restar(float numero1, float numero2){
+       float resultado = numero1-numero2;
+       return String.valueOf(resultado);
+    }
+    
+    public String Division(float numero1, float numero2){
+       float resultado = numero1/numero2;
+       return String.valueOf(resultado);
+    }
+    
+    public String Multiplicacion(float numero1, float numero2){
+       float resultado = numero1*numero2;
+       return String.valueOf(resultado);
+    }
+    
+}
